8901b53bb586beb8878f43c12773818955521941
Shawky <amamasa121212@gmail.com>
2023-11-30 17:48:05 -0800

    Making 3-infinite_add.py file executable

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
+
     import sys
 
     sum = 0
     counter = len(sys.argv) - 1
 
     for i in range(counter):
-        sum =+ int(sys.argv[i + 1])
+        sum = sum + int(sys.argv[i + 1])
 
     print(sum)
